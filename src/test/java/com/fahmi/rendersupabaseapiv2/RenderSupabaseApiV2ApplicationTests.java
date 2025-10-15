6034084e656bcd6a932a2986aeef10df555975cf
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-15 07:46:41 +0700

    initial commit

---
+++ src/test/java/com/fahmi/rendersupabaseapiv2/RenderSupabaseApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahmi.rendersupabaseapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderSupabaseApiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
